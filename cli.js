0f931f7cf4df92cb0a3573b41f7d15a353c06d79
Nicholas Carter <chaynick@email.unc.edu>
2022-11-18 15:10:14 -0500

    added latitude/longitude range messages

--- cli.js
+++ cli.js
@@ -6,6 +6,11 @@ import fetch from "node-fetch";
 var argv = minimist(process.argv.slice(2));
 const timezone = moment.tz.guess();
 
+if(argv["n"] >= 90 || argv["n"] <= -90){ console.log("Latitude must be in range"); process.exit(); }
+if(argv["s"] >= 90 || argv["s"] <= -90){ console.log("Latitude must be in range"); process.exit(); }
+if(argv["e"] >= 90 || argv["e"] <= -90){ console.log("Latitude must be in range"); process.exit(); }
+if(argv["w"] >= 90 || argv["w"] <= -90){ console.log("Latitude must be in range"); process.exit(); }
+
 if(argv["h"]){ 
     console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n    -h            Show this help message and exit.\n    -n, -s        Latitude: N positive; S negative.\n    -e, -w        Longitude: E positive; W negative.\n    -z            Time zone: uses tz.guess() from moment-timezone by default.\n    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n    -j            Echo pretty JSON from open-meteo API and exit.");
     process.exit(0);
